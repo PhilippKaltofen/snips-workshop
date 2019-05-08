a4f06f5ab0b2ba96d5f3cac3edd6c23600474e47
PhilippKaltofen <pkaltofen@yahoo.com>
2019-05-08 14:33:54 +0200

    Update action-how-are-you.py

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
+import random
 
-INTENT_HOW_ARE_YOU = "bezzam:how_are_you"
+INTENT_HOW_ARE_YOU = "PhilippKaltofen:how_are_you"
 
 
 def main():
@@ -10,10 +11,11 @@ def main():
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
          .start()
 
+responses = ["I'm not doing great", "I'm very well, thank you.", "Nice Bro, as always.", "What do you think?", "Dude, look at me.", "Please kill me."]
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "I'm doing great."
+    response = responses[random.randint(0, len(responses))]
     hermes.publish_end_session(session_id, response)
 
 
